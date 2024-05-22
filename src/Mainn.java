6b562ffe5fa307aeb780d9b516cbef50528f0e65
Katrin.tea <tea.cat.09.08.2005@gami.com>
2024-05-22 16:58:32 +0400

    add second task

---
+++ src/Mainn.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Mainn {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashMap<Integer, Integer> combinations = new HashMap<>();
+        combinations.put(1, 1);
+        combinations.put(2, 2);
+        combinations.put(3, 2);
+        combinations.put(0, 0);
+
+        long sum = 0L;
+        int count = scanner.nextInt();
+        for (int i = 1; i <= count; i++) {
+            int n = scanner.nextInt();
+            sum += n / 4;
+            sum += combinations.get(n % 4);
+        }
+        System.out.println(sum);
+    }
+}
